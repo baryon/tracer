8dd4968c92333ab1ca054da82ded67e0ecd4fe1f
LI Long <lilong@gmail.com>
2012-03-07 17:05:15 +0900

    add format3.js example

---
+++ example/format3.js
@@ -0,0 +1,15 @@
+var tracer = require('tracer')
+
+function test(id){
+	var logger = tracer.colorConsole(
+				{
+					format : "{{timestamp}} " + id + " <{{title}}> {{message}}",
+					dateformat : "HH:MM:ss.L"
+				});
+	logger.log('hello');
+	logger.trace('hello', 'world');
+	logger.debug('hello %s', 'world', 123);
+}
+
+test('ABC');
+test('123');
