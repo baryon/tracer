55b3a4c265b3128b86f2d8bfcab63a2361548a24
Long LI <lilong@gmail.com>
2014-05-16 15:56:34 +0900

    close stream

--- example/stream.js
+++ example/stream.js
@@ -3,11 +3,13 @@ var fs = require('fs');
 var logger = require('tracer').console({
 	transport : function(data) {
 			console.log(data.output);
-			var stream = fs.createWriteStream("./stream.log", {
+			var logStream = fs.createWriteStream("./stream.log", {
 			    flags: "a",
 			    encoding: "utf8",
 			    mode: 0666
-			}).write(data.output+"\n");
+			});
+			logStream.write(data.output+"\n");
+			logStream.end();
 		}
 });
 
@@ -17,3 +19,4 @@ logger.debug('hello %s',  'world', 123);
 logger.info('hello %s %d',  'world', 123, {foo:'bar'});
 logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'});
 logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object);
+
